5609e83c1464395bf9010d2b0a1c7f84d667092a
Abdel Khedr <abdelkhedr@Abdels-MacBook-Pro.local>
2022-08-14 17:49:24 -0400

    finished tempo

---
+++ main.js
@@ -0,0 +1,42 @@
+let start = false;
+let playBtn = document.getElementById("playBtn");
+let i = 0;
+const loop = new Tone.Loop(repeat, "8n").start(0);
+document.getElementById("playBtn").onclick = function () {
+  start = !start;
+  if (start) {
+    playBtn.innerHTML = "Stop";
+    Tone.Transport.start();
+  } else {
+    playBtn.innerHTML = "Play";
+    Tone.Transport.stop();
+  }
+};
+
+function repeat() {
+  const sound1 = new Tone.Player("Samples/clap-808.wav").toDestination();
+  const sound2 = new Tone.Player("Samples/kick-808.wav").toDestination();
+  const sound3 = new Tone.Player("Samples/hihat-808.wav").toDestination();
+  const sound4 = new Tone.Player("Samples/shaker-suckup.wav").toDestination();
+  const soundOneCurrId = "s1n" + ((i % 8) + 1);
+  const soundTwoCurrId = "s2n" + ((i % 8) + 1);
+  const soundThreeCurrId = "s3n" + ((i % 8) + 1);
+  const soundFourCurrId = "s4n" + ((i % 8) + 1);
+  if (document.getElementById(soundOneCurrId).checked) {
+    sound1.autostart = true;
+  }
+  if (document.getElementById(soundTwoCurrId).checked) {
+    // console.log("true");
+    sound2.autostart = true;
+  }
+  if (document.getElementById(soundThreeCurrId).checked) {
+    // console.log("true");
+    sound3.autostart = true;
+  }
+  if (document.getElementById(soundFourCurrId).checked) {
+    // console.log("true");
+    sound4.autostart = true;
+  }
+
+  i++;
+}
